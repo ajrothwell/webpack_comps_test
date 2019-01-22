35c0ef97326bfcba1b5f43cf048b311c42e2eb49
andy.rothwell <rothwell.andy@gmail.com>
2019-01-22 14:08:22 -0500

    adds font awesome stuff

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ console.log('square:', square);
 
 document.getElementById('test').innerHTML = square(10);
 
-// import '../node_modules/phila-standards/dist/css/phila-standards.min.css';
+import '../node_modules/phila-standards/dist/css/phila-standards.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faExternalLink } from '@fortawesome/pro-solid-svg-icons/faExternalLink';
+import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
+import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
+import { faTimesCircle } from '@fortawesome/pro-solid-svg-icons/faTimesCircle';
+import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
+import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
+library.add(faExternalLink, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 const store = createStore();
 
